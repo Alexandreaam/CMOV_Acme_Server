2b7b36101da57066ac5d8a28c1b2be291ab36b47
Alexandreaam <alex.al.mar@hotmail.com>
2020-11-15 15:05:58 +0000

    Added table data

--- cmov script.sql
+++ cmov script.sql
@@ -1,50 +1,57 @@
 create table users
 (
-    username   varchar(255),
-    password   varchar(255),
-    fullname   varchar(255),
-    creditcard integer,
-    nif        integer,
-    id         serial not null
+    username        varchar(255),
+    password        varchar(255),
+    fullname        varchar(255),
+    creditcard      integer,
+    nif             integer,
+    coffeecount     integer,
+    totalspendings  double precision,
+    userid          serial not null PRIMARY KEY
 );
 
 create unique index table_name_id_uindex
-    on users (id);
+    on users (userid);
 
 create unique index table_name_username_uindex
     on users (username);
+
+INSERT INTO users (username, password, fullname, creditcard, nif, coffeecount, totalspendings)
+VALUES ('admin', 'admin' , 'admin', '123', '123', '0', '0');
 
 create table products
 (
     title   	varchar(255),
     details 	varchar(255),
-    price 	varchar(255),
-    image 	varchar(255)
+    price 	    double precision,
+    image 	    varchar(255)
 );
 
 create table vouchers
 (
     title   	varchar(255),
-    details	varchar(255),
-    image 	varchar(255)
+    details	    varchar(255),
+    userid	    int REFERENCES users(userid),
+    image 	    varchar(255),
+    vouchid     serial not null PRIMARY KEY
 );
 
 INSERT INTO products (title, details, price, image)
-VALUES ('Panini', 'Hot pressed bread.' , '1.00€', 'images/Panini.webp');
+VALUES ('Panini', 'Hot pressed bread.' , '1.00', 'images/Panini.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Ham&Cheese sandwich', 'Hot pressed sliced bread with ham and cheese.' , '1.00€', 'images/HamCheese.webp');
+VALUES ('Ham&Cheese sandwich', 'Hot pressed sliced bread with ham and cheese.' , '1.00', 'images/HamCheese.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Croissant', 'Simple croissant.' , '1.20€', 'images/Croissant.webp');
+VALUES ('Croissant', 'Simple croissant.' , '1.20', 'images/Croissant.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Coffee', 'Hot cup of coffee.' , '0.50€', 'images/Coffee.webp');
+VALUES ('Coffee', 'Hot cup of coffee.' , '0.50', 'images/Coffee.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Pancakes', 'A stack of 5 pancakes.' , '5.40€', 'images/Pancakes.webp');
+VALUES ('Pancakes', 'A stack of 5 pancakes.' , '5.40', 'images/Pancakes.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Coffee with Milk', 'Hot cup of coffee with milk.' , '1.50€', 'images/CoffeeWithMilk.webp');
+VALUES ('Coffee with Milk', 'Hot cup of coffee with milk.' , '1.50', 'images/CoffeeWithMilk.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Chocolate Panike', 'Puff pastry with chocolate filling.' , '1.50€', 'images/PanikeChocolate.webp');
+VALUES ('Chocolate Panike', 'Puff pastry with chocolate filling.' , '1.50', 'images/PanikeChocolate.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Ham&Cheese Panike', 'Puff pastry with cheese and ham filling.' , '1.50€', 'images/PanikeHamCheese.webp');
+VALUES ('Ham&Cheese Panike', 'Puff pastry with cheese and ham filling.' , '1.50', 'images/PanikeHamCheese.webp');
 INSERT INTO products (title, details, price, image)
-VALUES ('Sausage Panike', 'Puff pastry with sausage and ham filling.' , '1.50€', 'images/PanikeSausage.webp');
+VALUES ('Sausage Panike', 'Puff pastry with sausage and ham filling.' , '1.50', 'images/PanikeSausage.webp');
 
